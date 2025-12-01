960ac24d7a776b4de81fddecb88747a0d6133db8
Cyrus Zheng <zhengc@email.sc.edu>
2025-11-30 21:26:31 -0500

    Added Team Logos

--- my-react-app/src/pages/PremierLeagueTeams.jsx
+++ my-react-app/src/pages/PremierLeagueTeams.jsx
@@ -6,10 +6,25 @@ const PremierLeagueTeams = () => {
             <h3 id="location">Soccer / Teams / Premier League</h3>
             <h2 id="quest">Premier League Teams</h2>
             <section id="premier-league-teams-section">
-                <Team img="../assets/Arsenal.png" name="Arsenal"/>
-                <Team img="../assets/Arsenal.png" name="Arsenal"/>
-                <Team img="../assets/Arsenal.png" name="Arsenal"/>
-                <Team img="../assets/Arsenal.png" name="Arsenal"/>
+                <Team img="https://upload.wikimedia.org/wikipedia/hif/8/82/Arsenal_FC.png" name="Arsenal"/>
+                <Team img="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Aston_Villa_FC_new_crest.svg/250px-Aston_Villa_FC_new_crest.svg.png" name="Aston Villa"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/bournemouth.vresize.72.72.medium.0.png" name="Bournemouth"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/brentford.vresize.72.72.medium.0.png" name="Brentford"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/brighton-hove-albion.vresize.72.72.medium.0.png" name="Brighton & Hove Albion"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/chelsea.vresize.72.72.medium.0.png" name="Chelsea"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/crystal-palace.vresize.72.72.medium.0.png" name="Crystal Palace"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/everton.vresize.72.72.medium.0.png" name="Everton"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/leeds-united.vresize.72.72.medium.0.png" name="Leeds United"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/leicester-city.vresize.72.72.medium.0.png" name="Leicester City"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/liverpool.vresize.72.72.medium.0.png" name="Liverpool"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/manchester-city.vresize.72.72.medium.0.png" name="Manchester City"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/manchester-united.vresize.72.72.medium.0.png" name="Manchester United"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/newcastle-united.vresize.72.72.medium.0.png" name="Newcastle United"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/norwich-city.vresize.72.72.medium.0.png" name="Norwich City"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/southampton.vresize.72.72.medium.0.png" name="Southampton"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/tottenham-hotspur.vresize.72.72.medium.0.png" name="Tottenham Hotspur"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/west-ham-united.vresize.72.72.medium.0.png" name="West Ham United"/>
+                <Team img="https://b.fssta.com/uploads/application/soccer/team-logos/wolverhampton.vresize.72.72.medium.0.png" name="Wolverhampton Wanderers"/>
             </section>
         </main>
     );
