92ac097829553bd4162d6c81728024fbb6f3e700
Ava Patel <avamp@email.sc.edu>
2025-12-03 14:59:45 -0500

    logo page

---
+++ my-react-app/src/assets/teamLogo.js
@@ -0,0 +1,66 @@
+// premier league team logos
+// src/data/teamLogos.js
+
+const teamLogos = {
+    "Arsenal FC":
+      "https://upload.wikimedia.org/wikipedia/hif/8/82/Arsenal_FC.png",
+  
+    "Aston Villa FC":
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Aston_Villa_FC_new_crest.svg/250px-Aston_Villa_FC_new_crest.svg.png",
+  
+    "AFC Bournemouth":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/bournemouth.vresize.72.72.medium.0.png",
+  
+    "Brentford FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/brentford.vresize.72.72.medium.0.png",
+  
+    "Brighton & Hove Albion FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/brighton-hove-albion.vresize.72.72.medium.0.png",
+  
+    "Burnley FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/burnley.vresize.72.72.medium.0.png",
+  
+    "Chelsea FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/chelsea.vresize.72.72.medium.0.png",
+  
+    "Crystal Palace FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/crystal-palace.vresize.72.72.medium.0.png",
+  
+    "Everton FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/everton.vresize.72.72.medium.0.png",
+  
+    "Leeds United FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/leeds-united.vresize.72.72.medium.0.png",
+  
+    "Liverpool FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/liverpool.vresize.72.72.medium.0.png",
+  
+    "Manchester City FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/manchester-city.vresize.72.72.medium.0.png",
+  
+    "Manchester United FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/manchester-united.vresize.72.72.medium.0.png",
+  
+    "Newcastle United FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/newcastle-united.vresize.72.72.medium.0.png",
+  
+    "Nottingham Forest FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/nottingham-forest.vresize.72.72.medium.0.png",
+  
+    "Sunderland AFC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/sunderland.vresize.72.72.medium.0.png",
+  
+    "Tottenham Hotspur FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/tottenham-hotspur.vresize.72.72.medium.0.png",
+  
+    "West Ham United FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/west-ham-united.vresize.72.72.medium.0.png",
+  
+    "Wolverhampton Wanderers FC":
+      "https://b.fssta.com/uploads/application/soccer/team-logos/wolverhampton.vresize.72.72.medium.0.png",
+
+    "Fulham FC":
+        "https://b.fssta.com/uploads/application/soccer/team-logos/fulham.vresize.72.72.medium.0.png",
+
+  };
+  export default teamLogos;
